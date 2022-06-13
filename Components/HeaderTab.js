545703b5018df0baf2fe40ee55831f2fdfedc51e
Decagon <decagon@Ekene.local>
2022-06-13 01:28:37 +0100

    Search Bar Component

--- Components/HeaderTab.js
+++ Components/HeaderTab.js
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function HeaderTab() {
+     const [activeTab, setActiveTab] = useState("Delivery");
   return (
     <View style={{
          flexDirection: "row",
          alignSelf: "center"
     }}>
-      <HeaderButton text="Delivery" btnColor="black" textColor="white"/>
-      <HeaderButton text="Pick Up" btnColor="white" textColor="black"/>
+      <HeaderButton text="Delivery" btnColor="black" textColor="white" activeTab={activeTab} setActiveTab={setActiveTab}/>
+      <HeaderButton text="Pick Up" btnColor="white" textColor="black" activeTab={activeTab} setActiveTab={setActiveTab}/>
     </View>
   )
 }
@@ -18,13 +19,16 @@ const HeaderButton = (props) => (
           marginTop: 50
      }}>
           <TouchableOpacity style={{
-               backgroundColor: props.btnColor,
+               backgroundColor: props.activeTab === props.text ? "black" : "white",
                paddingVertical: 6,
                paddingHorizontal: 16,
                borderRadius: 30
-          }}>
+          }}
+          onPress={() => props.setActiveTab(props.text)}
+          >
+               
                <Text style={{
-                    color: props.textColor,
+                    color: props.activeTab === props.text ? "white" : "black",
                     fontSize: 15,
                     fontWeight: "600"
                }}>{props.text}</Text>
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,11 +1,22 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import HeaderTab from '../Components/HeaderTab'
+import SearchBar from '../Components/SearchBar'
 
 export default function Home() {
   return (
-     <SafeAreaView>
-          <HeaderTab />
+     <SafeAreaView style={{
+          backgroundColor: "#eee",
+          // flex: 1
+          }}>
+               <View style={{
+                    backgroundColor: "white", 
+                    padding: 15
+               }}>
+                    <HeaderTab />
+                    <SearchBar />
+               </View>
+          
      </SafeAreaView>
   )
 }
